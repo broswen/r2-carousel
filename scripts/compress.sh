b49c5b331f6e3bfd3d89f47a35b71b7f9fde5815
Brad <broswen@gmail.com>
2024-05-04 16:27:19 -0500

    decrease compress script quality

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -2,5 +2,5 @@
 
 for filename in $1/*; do
 	echo "compressing $filename"
-	convert "$filename" -strip -interlace Plane -quality 80% "$filename"
+	convert "$filename" -strip -interlace Plane -quality 75% "$filename"
 done
