decb43d57f872288cd07959de7212abd340344c4
Brad <broswen@gmail.com>
2024-05-04 20:18:47 -0500

    compress to .webp

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-for filename in $1/*; do
-	echo "compressing $filename"
-	convert "$filename" -strip -interlace Plane -quality 75% "$filename"
+# usage
+# compress.sh /path/to/images/directory /path/to/output/directory
+
+for file in $1/*; do
+	echo "compressing $file"
+	filename=$(basename "$file")
+	filename="${filename%.*}"
+	convert "$file" -strip -interlace Plane -quality 60% "$2"/"$filename".webp
 done
